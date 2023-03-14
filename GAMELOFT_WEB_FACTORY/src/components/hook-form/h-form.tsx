6270fde4a0833cb5a7fcf9456e5dc1fba4cb1f26
SongLe <lesong0162@gmail.com>
2023-03-14 21:24:00 +0700

    add component

---
+++ GAMELOFT_WEB_FACTORY/src/components/hook-form/h-form.tsx
@@ -0,0 +1,27 @@
+// @flow
+import { type HTMLAttributes } from 'react';
+import { FormProvider, useFormContext } from 'react-hook-form';
+// import { useTranslation } from 'react-i18next';
+import { Collapse, Form } from 'react-bootstrap';
+
+type Props = HTMLAttributes<HTMLFormElement> & {
+  methods: object;
+  onSubmit: (values: object) => void;
+};
+
+export const HForm = ({ methods, onSubmit, onError, innerRef, ...props }: Props) => {
+  return (
+    <FormProvider {...methods}>
+      <Form
+        noValidate
+        onSubmit={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          return methods.handleSubmit(onSubmit, onError)(e);
+        }}
+        {...props}
+        ref={innerRef}
+      />
+    </FormProvider>
+  );
+};
